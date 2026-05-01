b9c520a3a2e8212d169f1feae18ebf10bcd2d732
tatsiana_shulha <t.shulha@symfa.com>
2026-05-02 01:08:55 +0200

    fix api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
+  order: "",
+  import: "",
+  bff: "",
+  cart: "",
 };
 
 export default API_PATHS;

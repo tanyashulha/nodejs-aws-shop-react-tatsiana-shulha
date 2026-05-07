fd11ff57b1755921dbaaf87f88fb56d79c80438b
tatsiana_shulha <t.shulha@symfa.com>
2026-05-07 18:21:03 +0200

    fix: enable SPA routing by serving index.html for 403 and 404 errors in CloudFront

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "",
-  import: "",
-  bff: "",
-  cart: "",
+  order: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
+  import: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
+  bff: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
+  cart: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;

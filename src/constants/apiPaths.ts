6fe637c927c9fd3ef4599cecb0d92ac45c5365d8
tatsiana_shulha <t.shulha@symfa.com>
2026-06-13 21:29:29 +0200

    Task 9: connect frontend to deployed Cart Service on Elastic Beanstalk

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
   import: "https://hsrj66ubsa.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://re8n3jy5qe.execute-api.eu-central-1.amazonaws.com/prod",
+  cart: "http://tanyashulha-cart-api-develop.eu-central-1.elasticbeanstalk.com",
 };
 
 export default API_PATHS;
